0012465fb956b9d5c95760935b8bb40b5887e699
Roman Safin <gordonfreeman2332@gmail.com>
2020-09-30 17:16:25 +0300

    menu-acco

--- js/slider.js
+++ js/slider.js
@@ -1,53 +1,38 @@
-const verticalAcco = () => {
-    const links = document.querySelectorAll(".menu-acco__trigger");
-    const body = document.querySelector('body');
-
-    // const calculateWidth = () => {
-    // };
-
-    function closeItem(activeElement) {
-        const activeText = activeElement.querySelector(".menu-acco__content");
-        activeText.style.width = "0px";
-        activeElement.classList.remove("active");
-    }
-
-    links.forEach(function (elem) {
-        elem.addEventListener("click", function (e) {
-            e.preventDefault();
-            const link = e.target.closest(".menu-acco__trigger");
-            const active = document.querySelector(".menu-acco__item.active");
-            if (active) {
-                closeItem(active);
-            }
-
-            if (!active || active.querySelector(".menu-acco__trigger") !== link) {
-                const current = link.closest(".menu-acco__item");
-                current.classList.add("active");
-                const currentText = current.querySelector(".menu-acco__content");
-                if (body.offsetWidth > 480) {
-                    currentText.style.width = calculateWidth() + 'px';
-
-                    // width = calculateWidth();
-                    // console.log(widthE)
-                } else {
-                    currentText.style.width = '100%';
-                }
-                
-            }
-        }
-
-    });
-
-    document.addEventListener("click", e => {
+$(function(){
+    $('.menu-acco__trigger').on('click', function(e) {
         e.preventDefault();
-        let activePerson = document.querySelector(".menu-acco__item.active");
-        const target = e.target;
-        console.log(target);
-        if (!target.closest("/menu-acco") && activePerson) {
-            closeItem(activePerson);
-        }
-        if(target.closest('.close-btn')){
-            closeItem(activePerson);
+        var $this = $(this),
+        container = $this.closest('.menu-acco'),
+        item = $this.closest('.menu-acco__item'),
+        items = container.find('.menu-acco__item'),
+        activeItem = items.filter('.active'),
+        content = item.find('.menu-acco__content'),
+        activeContent = activeItem.find('.menu-acco__content');
+
+        if (!item.hasClass('active')) {
+            items.removeClass('active');
+            item.addClass('active');
+            activeContent.animate ({
+                'width' : '0px'
+            });
+            content.animate ({
+                'widht' : '530px'
+            });
+        } else {
+            item.removeClass('active');
+            content.animate ({
+                'width' : '0px'
+            });
         }
     });
-};
+    $(document).on('click', function (e) {
+        var $this = $(e.target);
+        if (!$this.closest('.menu-acco').length) {
+            $('.menu-acco__content').animate ({
+                'width' : '0px'
+            });
+            $('.menu-acco__item').removeClass('active');
+        }
+    });
+});
+
